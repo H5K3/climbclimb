bf7aa15436f8885ff6be0728146b25167a178481
H5K3 <bumpybr617@gmail.com>
2020-06-12 18:52:25 +0900

    building

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,5 +1,5 @@
 Rails.application.configure do
-  confg.hosts.clear
+  config.hosts.clear
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
